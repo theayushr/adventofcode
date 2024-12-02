d6e6c27bf0d57066a33224392bf2df7f1a07fffc
Ayush Rawat <ayushrawat3338@gmail.com>
2024-12-02 22:13:28 +0530

    02Dec

---
+++ 2024/04.cpp
@@ -0,0 +1,73 @@
+/* ॐ */
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+const int N = 1000;
+
+void adventofcode();
+bool isSafe(vector<int> nums);
+bool dampener(vector<int> &nums);
+
+int32_t main() {
+	#ifndef ONLINE_JUDGE
+	freopen("./input.txt","r",stdin);
+	freopen("./output.txt","w",stdout);
+	#endif
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+
+	adventofcode();
+	return 0;
+}
+
+std::vector<std::string> split_string(const std::string &str, char delim = ' ') {
+    std::vector<std::string> tokens;
+    std::stringstream ss(str);
+    std::string token;
+    while (getline(ss, token, delim)) {
+        if (!token.empty())
+            tokens.push_back(token);
+    }
+    return tokens;
+}
+
+void adventofcode() {
+	int ans = 0;
+	for(int i=0; i<N; i++) {
+		string line;
+		std::getline(std::cin, line);
+		vector<string> numsStr = split_string(line, ' ');
+		vector<int> nums;
+		for(string ele : numsStr) nums.push_back(stoi(ele));
+		ans += isSafe(nums) or dampener(nums);
+	}
+	cout<<ans;
+}
+
+bool isSafe(vector<int> nums) {
+	if(nums.size() == 1) return true;
+	bool incr = (nums[1] - nums[0]) > 0;
+	for(int i=1; i<nums.size(); i++) {
+		int diff = nums[i] - nums[i-1];
+		if (incr && diff >= 1 && diff <= 3) continue;
+		if (!incr && diff <= -1 && diff >= -3 ) continue;
+		return false;
+	}
+	return true;
+}
+
+//tolerate a single bad level
+bool dampener(vector<int> &nums) {
+	for (int i =0; i<nums.size(); i++) {
+		int curr = nums[i];
+		nums.erase(nums.begin() + i);
+		if(isSafe(nums)) return true;
+		nums.insert(nums.begin() + i, curr);
+	}
+	return false;
+}
